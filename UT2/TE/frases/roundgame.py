d3c3d68fe25b2e1a881d8a05e5fa277d860ee306
v1ct0rjs <vmjimenezs02@educarex.es>
2024-02-26 09:22:36 +0100

    Trabajo evalueble 2.

--- UT2/TE/frases/roundgame.py
+++ UT2/TE/frases/roundgame.py
@@ -1,5 +1,18 @@
+from .player import Player
+from .frase import Phrase
+
+
 class RoundGame():
 
-    def __init__(self):
+    def __init__(self, players: list, phrases: int):
+        self.players = players
+        self.phrases = phrases
+        self.currentPhrase = None
+        self.currentPlayer = None
 
-        pass
+    def playRound(self):
+        for player in self.players:
+            self.currentPlayer = player
+            self.currentPhrase = Phrase.getPhrase()
+            self.currentPhrase.showPhrase()
+            self.currentPlayer.guessPhrase(self.currentPhrase)
