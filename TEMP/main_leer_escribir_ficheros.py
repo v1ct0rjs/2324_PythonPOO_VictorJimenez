180194fd11f6171c61e475f2c60515d970a9215c
v1ct0rjs <vmjimenezs02@educarex.es>
2024-03-12 19:13:33 +0100

    Carpeta Temp con funciones para trabajar ficheros

--- TEMP/main_leer_escribir_ficheros.py
+++ TEMP/main_leer_escribir_ficheros.py
@@ -27,9 +27,30 @@ def abrirFicheroConWith(filename: str):
     except Exception as e:
         print(e)
 
+def copyFile(sourceFile:str, targetFile: str):
+    contenido = ""
+    with open(sourceFile, mode='r') as sfile:
+        # Leo contenido
+        contenido = sfile.read()
 
+    with open(targetFile, mode='a') as tfile:
+        tfile.write(f'{contenido}\n')
+
+    print("Fichero copiado correctamente")
+
+def copyFileLineaLinea(sourceFile:str, targetFile: str):
+    contenido = ""
+    with open(sourceFile, mode='r') as sfile:
+        with open(targetFile, mode='w') as tfile:
+            for linea in sfile:
+                tfile.write(f'{linea}\n')
+
+
+
+    print("Fichero copiado correctamente")
 
 if __name__ == '__main__':
     # abrifichero("temp.dat")
-    abrirFicheroConWith("temp.dat")
-
+    # abrirFicheroConWith("temp.dat")
+    copyFile('temp.dat', 'temp_bk.dat')
+    copyFileLineaLinea('temp.dat', 'temp_bk_2.dat')
